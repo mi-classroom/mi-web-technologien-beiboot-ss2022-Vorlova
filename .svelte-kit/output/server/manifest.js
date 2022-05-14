38c5869adfb4c6ca2b92083858247c9b7f6797cd
Valeria Orlova <v.orlova@sprinteins.com>
2022-05-14 13:36:12 +0200

    build: app

--- .svelte-kit/output/server/manifest.js
+++ .svelte-kit/output/server/manifest.js
@@ -3,7 +3,7 @@ export const manifest = {
 	assets: new Set(["favicon.png"]),
 	mimeTypes: {".png":"image/png"},
 	_: {
-		entry: {"file":"start-5785a037.js","js":["start-5785a037.js","chunks/index-5ea7147a.js"],"css":[]},
+		entry: {"file":"start-03226509.js","js":["start-03226509.js","chunks/index-5ea7147a.js"],"css":[]},
 		nodes: [
 			() => import('./nodes/0.js'),
 			() => import('./nodes/1.js'),
