794bb212950a995cbe56bceeb9a2dd0604f82f87
Valeria Orlova <v.orlova@sprinteins.com>
2022-06-13 22:26:03 +0200

    feat: add years to view

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -8,30 +8,36 @@
 
 	let files: any;
 	let images: any[] = [];
+	let years: any[] = [];
+	let itemYear: string[];
+	const font = new Font( OpenFont );
 
 	// debug
 	images.push({
 		src: 'https://cdn.pixabay.com/photo/2014/04/13/20/49/cat-323262_1280.jpg',
 		title: 'Title 1',
+		date: '2022'
 	},
 	{
 		src: 'https://images.pexels.com/photos/6447547/pexels-photo-6447547.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
 		title: 'Title 2',
+		date: 'um 2022'
 	});
 
 	const loader = new THREE.TextureLoader();
 
 	// define timeline base
 	const lineMaterial = new THREE.LineBasicMaterial( { color: 0x222222 } );
-	let lineGeometry = new THREE.BoxGeometry( 100, 0.5, 0.5, 10, 10, 10);
 
 	// define ground plane for orientation
 	const groundMaterial = new THREE.MeshStandardMaterial({ color: 0xb4b4b4 });
-	let groundGeometry = new THREE.BoxGeometry( 1500, 0, 1500);
+	let groundGeometry = new THREE.BoxGeometry( 2048, 0, 2048);
 
 	// define image planes
 	let imageGeometry = new THREE.BoxGeometry( 20, 40, 1);
-	let imagePosition: [number, number, number] = [-80, 50, 15];
+	let imagePosition: [number, number, number] = [-1000, 50, 15];
+
+	const cameraPosition: [number, number, number] = [-1020, 50, 15];
 
 	//Signs:
 	const signGeometry = new THREE.BoxGeometry( 35, 15, 1);let signPosition: [number, number, number] = [imagePosition[0], imagePosition[1]-30, imagePosition[2]];
@@ -52,9 +58,8 @@
 		new THREE.MeshBasicMaterial({ color: 0xffffff }),
 	];
 
-	// Texts
+	// Sign Texts
 	let textGeometries: any[] = [];
-	const font = new Font( OpenFont );
 	const generateSignText = (image: any) => {
 		const text: string = 'Title: '+image.title;
 		textGeometries.push( new TextGeometry( text, {
@@ -63,7 +68,11 @@
 			height: 1,
 		} ));
 	}
-	let textPosition: [number, number, number] = [ signPosition[0], signPosition[1]+2, signPosition[2]-16];
+	let textPosition: [number, number, number] = [ signPosition[0], signPosition[1]+2, signPosition[2]-16 ];
+
+	// Year 3d Texts
+	let yearGeometries: any[] = [];
+	let yearPosition: [number, number, number] = [ imagePosition[0], imagePosition[1], imagePosition[2]-50 ];
 
 	let materials: any[] = [];
 	// TODO: define image type
@@ -98,9 +107,12 @@
 	const resetData = () => {
 		images = [];
 		textGeometries = [];
-		imagePosition = [-80, 50, 15];
+		imagePosition = [-1000, 50, 15];
 		signPosition = [imagePosition[0], imagePosition[1]-30, imagePosition[2]];
 		textPosition = [ signPosition[0], signPosition[1]+2, signPosition[2]-16];
+		yearGeometries = [];
+		years = [];
+		yearPosition = [ imagePosition[0], imagePosition[1], imagePosition[2]-50 ];
 	}
 
 	let handleSubmit = () => {
@@ -115,6 +127,21 @@
 			}
 		);
 	}
+
+	const cleanUpYear = (itemDate: string) => {
+		itemYear = itemDate.split(' ')
+		if (itemYear.length > 1) {
+			if( !isNaN(
+				parseInt(itemYear[0])
+			)) {
+				years.push(itemYear[0])
+			} else {
+				years.push(itemYear[1])
+			}
+		} else {
+			years.push(itemDate)
+		}
+	}
 </script>
 
 <form on:submit|preventDefault={handleSubmit}>
@@ -122,11 +149,11 @@
 	     so the page won't reload -->
 	<label for="source-file">Upload a JSON source for the images:</label>
 	<input
-	accept="application/JSON"
-	bind:files
-	id="source-file"
-	name="source-file"
-	type="file"
+		accept="application/JSON"
+		bind:files
+		id="source-file"
+		name="source-file"
+		type="file"
 	/>
 	<button type="submit">Use this file</button>
 </form>
@@ -137,16 +164,34 @@
 		antialias
 		background={new THREE.Color(180, 180, 180)}
 	>
-		<SC.PerspectiveCamera position={[-100, 50, 75]} target={[10, 25, 0]}/>
+		<SC.PerspectiveCamera position={cameraPosition} target={ [10, 25, 0] }/>
 		<SC.OrbitControls enableZoom={true} enableRotate={true}/>
 		<SC.AmbientLight intensity={1} />
 		<SC.DirectionalLight intensity={0.6} position={[-2, 3, 2]} shadow={{ mapSize: [2048, 2048] }}/>
 
 		<SC.Mesh geometry={groundGeometry} material={groundMaterial} position={[0, 0, 0]} receiveShadow/>
-		<SC.Mesh geometry={lineGeometry} material={lineMaterial}  position={[0, 1, 0]} />
 
 		{#each images as item, index}
 			{loadImageTextures(item)}
+			{#if item.date != undefined}
+				{cleanUpYear(item.date)}
+				{yearGeometries.push(
+					new TextGeometry(
+						years[index],
+						{
+							font: font,
+							size: 5,
+							height: 2,
+						},
+					)
+				)}
+			{/if}
+			<SC.Mesh
+				geometry={yearGeometries[index]}
+				material={lineMaterial}
+				position={yearPosition}
+				rotation={[0, Math.PI / -2, 0]}
+			/>
 			<SC.Mesh
 				geometry={imageGeometry}
 				material={materials[index]}
@@ -168,9 +213,10 @@
 				position={textPosition}
 				rotation={[0, Math.PI / -2, 0]}
 			/>
-			{imagePosition[0]=imagePosition[0]+30}
-			{signPosition[0]=signPosition[0]+30}
-			{textPosition[0]=textPosition[0]+30}
+			{imagePosition[0]=imagePosition[0]+150}
+			{signPosition[0]=signPosition[0]+150}
+			{textPosition[0]=textPosition[0]+150}
+			{yearPosition[0]=yearPosition[0]+150}
 		{/each}
 	</SC.Canvas>
 </div>
